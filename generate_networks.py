c134c83532b9f3ffe6a884babaa01caa74b2f930
mehmetdirenc <mehmetdirencmungan@gmail.com>
2024-08-20 17:46:53 +0200

    rnaseq intersections and R reworking

--- generate_networks.py
+++ generate_networks.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import py4cytoscape as p4c
 from time import sleep
-from py4cytoscape import CyError
+import os
 pd.options.mode.chained_assignment = None
 p4c.cytoscape_version_info()
+from itertools import combinations
+from pyvenn import *
 
 
 def get_deseq_results(exp_filepath, genes_to_keep):
@@ -23,37 +25,22 @@ def get_network_name():
 def create_network(center_gene, specie, exp_name, exp_filepath, genes_to_keep):
     available_networks = p4c.get_network_list()
     main_network_name = center_gene + "_" + exp_name
-    # if main_network_name in available_networks:
-    #     p4c.set_current_network(main_network_name)
-    #     sleep(0.5)
-    # else:
+    if "STRING network - %s"%main_network_name in available_networks:
+        return
     string_cmd_list = ['string protein query', 'query="', center_gene, '"', 'species=%s' % specie, 'cutoff=0.4',
                        "newNetName=%s" % main_network_name, "limit=1000"]
     string_cmd = " ".join(string_cmd_list)
     p4c.commands.commands_run(string_cmd)
-    # p4c.set_current_network(main_network_name)
     sleep(0.5)
     string_cmd = 'string retrieve enrichment allNetSpecies="%s"'%specie
     p4c.commands.commands_run(string_cmd)
     sleep(0.5)
-        # p4c.set_visual_style("de_genes_style")
-    # mapped_cols = p4c.map_table_column('display name', specie, 'HGNC', 'Entrez Gene')
-    # subnetwork_name = center_gene + "_" + exp_name
-    # p4c.select_nodes(by_col= "display name" ,nodes=center_gene)
-    # sleep(0.5)
-    # p4c.select_first_neighbors()
-    # p4c.create_subnetwork(nodes="selected", subnetwork_name=subnetwork_name)
-    # sleep(0.5)
     deseq_df = get_deseq_results(exp_filepath, genes_to_keep)
     p4c.load_table_data(deseq_df, data_key_column='gene_id', table_key_column='display name')
-    # p4c.export_
     string_cmd = 'string retrieve enrichment allNetSpecies="%s"'%specie
     p4c.commands.commands_run(string_cmd)
     sleep(0.5)
-    # string_net = p4c.get_network_suid()
-    # p4c.create_column_filter('log2filter', 'log2FoldChange', "", "IS_NOT", network=string_net)
-    # p4c.apply_filter('log2filter', hide=True, network=string_net)
-    p4c.set_visual_style("de_genes_style")
+    # p4c.set_visual_style("de_genes_style")
     return
 
 
@@ -68,16 +55,19 @@ def get_tables():
         if exp not in exp_to_table:
             exp_to_table[exp] = {}
         p4c.set_current_network(network)
-        sleep(0.2)
+        sleep(0.1)
         df = p4c.get_table_columns(columns=["gene_id", "log2FoldChange", ])
         df.dropna(subset=["log2FoldChange"], inplace=True)
         exp_to_table[exp][gene] = df["gene_id"].tolist()
     return exp_to_table
 
-def exp_intersection(gene_bool_dict):
+def exp_intersection(gene_bool_dict, main_res_folder):
     exp_to_table = get_tables()
-    intersection_list = []
     for exp in exp_to_table:
+        intersection_list = []
+        if exp == "rt_nrt":
+            print("asd")
+        create_venn(exp_to_table[exp], exp, main_res_folder)
         for gene in gene_bool_dict:
             if gene_bool_dict[gene]:
                 inclusion_list = exp_to_table[exp][gene]
@@ -87,7 +77,52 @@ def exp_intersection(gene_bool_dict):
             if not gene_bool_dict[gene]:
                 exclusion_list = exp_to_table[exp][gene]
                 final_genes_intersection = list(set(genes_intersection) - set(exclusion_list))
+        write_gene_list(final_genes_intersection, main_res_folder, exp)
     return
+
+def exp_intersection_all_combinations(gene_bool_dict, main_res_folder, areas_of_interest):
+    gene_set = [i for i in gene_bool_dict.keys() if gene_bool_dict[i] == True]
+    exp_to_table = get_tables()
+
+    for exp in exp_to_table:
+        all_collections = create_venn(exp_to_table[exp], exp, main_res_folder)
+        write_gene_list_all_collections(all_collections, main_res_folder, exp, areas_of_interest)
+        # current_exp_dict = exp_to_table[exp]
+        # current_exp_dict_set = {key: set(value) for key, value in current_exp_dict.items()}
+        # for i in gene_bool_dict.keys():
+        #     if gene_bool_dict[i] == False:
+        #         current_exp_dict_set.pop(i)
+        # intersections = {}
+        # for i in range(1, len(current_exp_dict_set) + 1):
+        #     for combo in combinations(current_exp_dict_set.keys(), i):
+        #         intersected_genes = set.intersection(*(current_exp_dict_set[set_name] for set_name in combo))
+        #         for gene in gene_bool_dict:
+        #             if not gene_bool_dict[gene]:
+        #                 exclusion_list = exp_to_table[exp][gene]
+        #                 final_genes_intersection = list(intersected_genes - set(exclusion_list))
+        #         intersections[combo] = final_genes_intersection
+        #
+        # write_gene_list(intersections, main_res_folder, exp)
+    return
+
+
+
+
+def create_venn(exp_dict, exp, main_res_folder):
+    res_filepath = os.path.join(main_res_folder, "venn", exp)
+    exp_dict_set = {}
+    genes = []
+    exp_dict = dict(sorted(exp_dict.items()))
+    for gene in exp_dict:
+        exp_dict_set[gene] = set(exp_dict[gene])
+        genes.append(gene)
+    labels1 = list(exp_dict_set.values())
+    labels, all_collections = get_labels(labels1)
+    fig, ax = venn4(labels, names=list(exp_dict_set.keys()))
+    # plt.show()
+    plt.savefig(res_filepath)
+    plt.close()
+    return all_collections
 
 def loop_experiments(experiments, specie, exp_filepath_variant, genes_to_keep):
     for center_gene in genes_to_keep:
@@ -95,12 +130,75 @@ def loop_experiments(experiments, specie, exp_filepath_variant, genes_to_keep):
             exp_filepath = exp_filepath_variant%(exp_name, exp_name)
             create_network(center_gene, specie, exp_name, exp_filepath, genes_to_keep)
 
+def write_gene_list(intersection_combo, main_res_folder, exp):
+    result_filepath = os.path.join(main_res_folder, "gene_lists", exp + ".tsv")
+    # with open(result_filepath, 'w') as rf:
+    max_length = max(len(lst) for lst in intersection_combo.values())
+    intersection_combo2 = {}
+    # Pad lists with None to have the same length
+    for key in intersection_combo:
+        new_key = "_".join(key) + "_" + str(len(intersection_combo[key]))
+        intersection_combo2[new_key] = intersection_combo[key] + [None] * (max_length - len(intersection_combo[key]))
+
+    # Convert the dictionary to a DataFrame
+    intersection_df = pd.DataFrame(intersection_combo2).fillna("")
+    # intersection_df = pd.DataFrame(intersection_combo.items())
+    # rf.write("#Gene\n")
+    intersection_df.to_csv(result_filepath, index=False, sep="\t")
+        # for i in intersection_combo:
+        #     rf.write("%s\t"%i)
+        # rf.write("\n")
+        # for gene in gene_list:
+        #     rf.write("%s\n" % gene)
+
+
+def write_gene_list_all_collections(all_collections, main_res_folder, exp, areas_of_interest):
+    result_filepath = os.path.join(main_res_folder, "gene_lists", exp + ".tsv")
+    intersection = {}
+    for area_name in areas_of_interest:
+        all_genes = []
+        for area in areas_of_interest[area_name]:
+            for gene in all_collections[area]:
+                all_genes.append(gene)
+        intersection[area_name] = all_genes
+    # with open(result_filepath, 'w') as rf:
+    # max_length = max(len(lst) for lst in intersection_combo.values())
+    # intersection_combo2 = {}
+    # # Pad lists with None to have the same length
+    # for key in intersection_combo:
+    #     new_key = "_".join(key) + "_" + str(len(intersection_combo[key]))
+    #     intersection_combo2[new_key] = intersection_combo[key] + [None] * (ma   x_length - len(intersection_combo[key]))
+
+    # Convert the dictionary to a DataFrame
+    pad_dict_list(intersection, "")
+    intersection_df = pd.DataFrame(intersection)
+    # intersection_df = pd.DataFrame(intersection_combo.items())
+    # rf.write("#Gene\n")
+    intersection_df.to_csv(result_filepath, index=False, sep="\t")
+        # for i in intersection_combo:
+        #     rf.write("%s\t"%i)
+        # rf.write("\n")
+        # for gene in gene_list:
+        #     rf.write("%s\n" % gene)
+
+def pad_dict_list(dict_list, padel):
+    lmax = 0
+    for lname in dict_list.keys():
+        lmax = max(lmax, len(dict_list[lname]))
+    for lname in dict_list.keys():
+        ll = len(dict_list[lname])
+        if  ll < lmax:
+            dict_list[lname] += [padel] * (lmax - ll)
+    return dict_list
+
 if __name__ == '__main__':
-    # experiments = ["r_nr", "nr_nrt", "r_rt", "rt_nrt"]
+    # experiments = ["nr_r", "nr_nrt", "r_rt", "nrt_rt"]
     # genes_to_keep = ['Braf', 'Akt1', 'Kras', 'Src']
     # exp_filepath_variant = "/home/direnc/results/tim/rnaseq_mice/%s/differential_abundance/tables/differential/condition_%s.deseq2.results.tsv"
     # specie = "Mus musculus"
     # loop_experiments(experiments, specie, exp_filepath_variant, genes_to_keep)
-    tables_folder = "/home/direnc/results/tim/rnaseq_mice/gene_centered_results"
+    main_res_folder = "/home/direnc/results/tim/rnaseq_mice/gene_centered_results"
     gene_bool_dict = {"Kras" : True , "Akt1" : True , "Src" : True , "Braf" : False}
-    exp_intersection(gene_bool_dict)
+    areas_of_interest = {"kras" : ["0010"], "kras_src" : ["0010", "0011"]}
+    # exp_intersection(gene_bool_dict, main_res_folder)
+    exp_intersection_all_combinations(gene_bool_dict, main_res_folder, areas_of_interest)
--- gsea.R
+++ gsea.R
@@ -1,44 +1,83 @@
-if (!("clusterProfiler" %in% installed.packages())) {
-  # Install this package if it isn't installed yet
-  BiocManager::install("clusterProfiler", update = FALSE)
+library(geneset)
+library(genekitr)
+library(tidyverse)
+library(dplyr)
+library(readr)
+# gskegg <- getKEGG(org = "mmu",category = "pathway")
+# gsmsigdb <- getMsigdb(org = "mouse", category = "C5-HPO")
+# gswiki <- getWiki(org = "mouse")
+# gsenrich <- getEnrichrdb(org = "human", library = "Cancer_Cell_Line_Encyclopedia")
+# enrichr_metadata_frame <- enrichr_metadata
+
+
+# Function to handle multiple gene sets
+process_multiple_gene_sets <- function(base_dir, exp_names, gs, type, p_cutoff = 0.05, q_cutoff = 0.05)
+{
+  for (exp_name in exp_names)
+  {
+    if (type == "go")
+    {
+      go_terms_revigo_file <- sprintf("%s/gene_centered_results/go_lists/Revigo_goterms_%s.tsv",
+                                base_dir, exp_name)
+    }
+    else
+    {
+      go_terms_revigo_file <- ""
+    }
+      dge_results_file <- sprintf("%s/%s/differential_abundance/tables/differential/condition_%s.deseq2.results_filtered.tsv",
+                                base_dir, exp_name, exp_name)
+    process_gene_set(dge_results_file, gs, p_cutoff, q_cutoff,exp_name, base_dir, go_terms_revigo_file, type)
+  }
 }
 
-if (!("msigdbr" %in% installed.packages())) {
-  # Install this package if it isn't installed yet
-  BiocManager::install("msigdbr", update = FALSE)
+
+process_gene_set <- function(dge_results_file, gs, p_cutoff = 0.05, q_cutoff = 0.05, exp_name, base_dir, go_terms_revigo_file, type)
+{
+  # Read the data
+  dge_df <- read_tsv(dge_results_file, show_col_types = FALSE)
+
+  # Subset and rename columns
+  dge_df <- subset(dge_df, select = -c(baseMean, lfcSE, pvalue, padj))
+  colnames(dge_df) <- c("geneID_symbol", "logfc")
+  dge_df <- dge_df %>% arrange(desc(logfc))
+  genes_of_interest <- setNames(dge_df$logfc, dge_df$geneID_symbol)
+  x <- typeof(genes_of_interest)
+  x
+  head(genes_of_interest)
+  # colnames(genes_of_interest) <- c("logfc", "geneID_symbol")
+  gsego <- genGSEA(genelist = genes_of_interest, geneset = gs, p_cutoff = p_cutoff, q_cutoff = q_cutoff)
+  if (type == "go")
+  {
+    gsego <- filter_gsea_by_go_terms(gsego, go_terms_revigo_file)
+  }
+  # x <- typeof(gsego$gsea_df)
+  # y <- typeof(genes_of_interest)
+  # plotGSEA(gsego, plot_type = "bar", colour = c("blue", "red"))
+  # ggsave(filename = paste0(sprintf("%s/gene_centered_results/go_figures/%s_gsea_plot.png",base_dir,exp_name)),
+  #        plot = last_plot(), width = 8, height = 6, dpi = 600)
+  # gene_list <- c("Apc","Bub1b","Cdx2","Elavl1","Fpr2","Ptprt","Tlr2","Trp53inp1", "Kras", "Braf", "Akt1", "Src")
+  # gsego$gsea_df$geneID_symbol <- gsego$gsea_df$geneID
+  plotGSEA(gsego, plot_type = "bar")
+  # plotEnrich(gsego$gsea_df, plot_type = "geneheat", fold_change = genes_of_interest)
+  ggsave(filename = paste0(sprintf("%s/gene_centered_results/%s_figures/%s_gsea_plot2.png",base_dir,type,exp_name)),
+         plot = last_plot())
 }
 
-if (!("org.Mm.eg.db" %in% installed.packages())) {
-  # Install this package if it isn't installed yet
-  BiocManager::install("org.Mm.eg.db", update = FALSE)
+
+filter_gsea_by_go_terms <- function(gsego, go_terms_file) {
+  # Read the GO terms from the external file
+  go_terms_to_keep <- read_tsv(go_terms_file, show_col_types = FALSE)$TermID
+
+  # Filter the gsea_df table in the gsego object
+  gsego$gsea_df <- gsego$gsea_df %>%
+    filter(mmusculus_BP_ID %in% go_terms_to_keep)
+
+  return(gsego)
 }
-# Attach the library
-library(clusterProfiler)
 
-# Package that contains MSigDB gene sets in tidy format
-library(msigdbr)
-
-# Human annotation package we'll use for gene identifier conversion
-library(org.Mm.eg.db)
-
-# We will need this so we can use the pipe: %>%
-library(magrittr)
-
-# Check if the results file exists
-dge_results_file <- "/home/direnc/results/tim/rnaseq_mice/nr_nrt/differential_abundance/tables/differential/condition_control_treated.deseq2.results.tsv"
-print("asd")
-file.exists(dge_results_file)
-# Read in the contents of the differential expression results file
-dge_df <- readr::read_tsv(dge_results_file)
-# Read in the contents of the differential expression results file
-dge_df <- readr::read_tsv(dge_results_file)
-library("msigdbr")
-mm_hallmark_sets <- msigdbr(
-  species = "Mus musculus", # Replace with species name relevant to your data
-  category = "H"
-)
-# msigdbr_species()
-all_gene_sets <- msigdbr(species = "Mus musculus")
-colnames(dge_df)[1] <- "gene_symbol"
-
-keytypes(org.Mm.eg.db)
+gsgo <- getGO(org = "mouse",ont = "bp")
+gskegg <- getKEGG(org = "mmu",category = "pathway")
+base_dir <- "/home/direnc/results/tim/rnaseq_mice"
+exp_names <- c("nr_r", "nr_nrt", "r_rt", "nrt_rt")
+type <- "kegg"
+process_multiple_gene_sets(base_dir, exp_names, gskegg, type)
